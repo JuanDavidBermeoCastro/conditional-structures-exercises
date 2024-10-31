f4a600fec6b4969e9f3c33c98cf47cf64c6168bb
JuanDavidBermeoCastro <186082659+JuanDavidBermeoCastro@users.noreply.github.com>
2024-10-31 08:42:51 -0500

    programa de division si es exacta o no

---
+++ division.py
@@ -0,0 +1,8 @@
+num1=int(input("enter a one number to perform the division: "))
+num2=int(input("enter a second number to perform the division: "))
+
+if (num1%num2==0):
+    print(f"this division is exact")
+    
+elif (num1%num2!=0):
+ print(f"this division is not exact ")
