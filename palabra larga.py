8b8cf66f16161639491c11c731101f4827d1dc93
JuanDavidBermeoCastro <186082659+JuanDavidBermeoCastro@users.noreply.github.com>
2024-10-31 15:19:45 -0500

    palabra larga

---
+++ palabra larga.py
@@ -0,0 +1,16 @@
+palabra1 = input("Palabra 1: ")
+palabra2 = input("Palabra 2: ")
+
+# Calcular la longitud de cada palabra
+longitud1 = len(palabra1)
+longitud2 = len(palabra2)
+
+# Comparar las longitudes y mostrar el resultado
+if longitud1 > longitud2:
+    diferencia = longitud1 - longitud2
+    print(f"La palabra {palabra1} tiene {diferencia} letras más que {palabra2}.")
+elif longitud2 > longitud1:
+    diferencia = longitud2 - longitud1
+    print(f"La palabra {palabra2} tiene {diferencia} letras más que {palabra1}.")
+else:
+    print("Las dos palabras tienen el mismo largo.")
